0c56ef6ad2939fded51e4dbb6cb2bb0de1748464
yyamazaki933 <yudai.yamazaki@map4.jp>
2022-12-29 19:19:55 +0900

    bug fix

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 sudo cp $SCRIPT_DIR/img/rosbag_player.png /usr/share/pixmaps/
 cat $SCRIPT_DIR/desktop/rosbag_player.desktop | sed -e "s?PATH?$SCRIPT_DIR?" > ~/.local/share/applications/rosbag_player.desktop
 
-ROW="application/vnd.sqlite3=rosbag_player.desktop"
-sed -i -e '/^$/i'$ROW';' $HOME/.config/mimeapps.list
-sed -i -e '$a'$ROW $HOME/.config/mimeapps.list
+if ! grep -q "rosbag_player.desktop" $HOME/.config/mimeapps.list; then
+    ROW="application/vnd.sqlite3=rosbag_player.desktop"
+    sed -i -e '/^$/i'$ROW';' $HOME/.config/mimeapps.list
+    sed -i -e '$a'$ROW $HOME/.config/mimeapps.list
+fi
