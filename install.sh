bb8ce18653851beaab74de77a17644cf04c435f0
yudai.yamazaki <yudai.yamazaki@map4.jp>
2023-07-08 21:24:48 +0900

    install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
+pip install PyYAML PyQt5
+
 sudo cp $SCRIPT_DIR/img/rosbag_player.png /usr/share/pixmaps/
 cat $SCRIPT_DIR/desktop/rosbag_player.desktop | sed -e "s?PATH?$SCRIPT_DIR?" > ~/.local/share/applications/rosbag_player.desktop
 
